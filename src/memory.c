9b291a9b10bdd1f79d529cca84fb5f9f83c06e37
MichaelSDavid <349612929@gapps.yrdsb.ca>
2020-10-12 19:52:26 -0400

    Added basic OOP

--- src/memory.c
+++ src/memory.c
@@ -72,6 +72,12 @@ static void blackenObject(Obj* object) {
 #endif
 
   switch (object->type) {
+    case OBJ_CLASS: {
+      ObjClass* class = (ObjClass*)object;
+      markObject((Obj*)class->name);
+      break;
+    }
+
     case OBJ_CLOSURE: {
       ObjClosure* closure = (ObjClosure*)object;
       markObject((Obj*)closure->function);
@@ -85,6 +91,13 @@ static void blackenObject(Obj* object) {
       ObjFunction* function = (ObjFunction*)object;
       markObject((Obj*)function->name);
       markArray(&function->chunk.constants);
+      break;
+    }
+
+    case OBJ_INSTANCE: {
+      ObjInstance* instance = (ObjInstance*)object;
+      markObject((Obj*)instance->class);
+      markTable(&instance->fields);
       break;
     }
 
@@ -104,6 +117,11 @@ static void freeObject(Obj* object) {
 #endif
 
   switch (object->type) {
+    case OBJ_CLASS: {
+      FREE(ObjClass, object);
+      break;
+    } 
+
     case OBJ_CLOSURE: {
       ObjClosure* closure = (ObjClosure*)object;
       FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
@@ -115,6 +133,13 @@ static void freeObject(Obj* object) {
       ObjFunction* function = (ObjFunction*)object;
       freeChunk(&function->chunk);
       FREE(ObjFunction, object);
+      break;
+    }
+
+    case OBJ_INSTANCE: {
+      ObjInstance* instance = (ObjInstance*)object;
+      freeTable(&instance->fields);
+      FREE(ObjInstance, object);
       break;
     }
 
--- src/object.c
+++ src/object.c
@@ -25,6 +25,12 @@ static Obj* allocateObject(size_t size, ObjType type) {
   return object;
 }
 
+ObjClass* newClass(ObjString* name) {
+  ObjClass* klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
+  klass->name = name; 
+  return klass;
+}
+
 ObjClosure* newClosure(ObjFunction* function) {
   ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*, function->upvalueCount);
   for (int i = 0; i < function->upvalueCount; i++) {
@@ -46,6 +52,13 @@ ObjFunction* newFunction() {
   function->name = NULL;
   initChunk(&function->chunk);
   return function;
+}
+
+ObjInstance* newInstance(ObjClass* class) {
+  ObjInstance* instance = ALLOCATE_OBJ(ObjInstance, OBJ_INSTANCE);
+  instance->class = class;
+  initTable(&instance->fields);
+  return instance;
 }
 
 ObjNative* newNative(NativeFn function) {
@@ -121,12 +134,20 @@ static void printFunction(ObjFunction* function) {
 
 void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
+    case OBJ_CLASS:
+      printf("%s", AS_CLASS(value)->name->chars);
+      break;
+
     case OBJ_CLOSURE:
       printFunction(AS_CLOSURE(value)->function);
       break;
 
     case OBJ_FUNCTION:
       printFunction(AS_FUNCTION(value));
+      break;
+
+    case OBJ_INSTANCE:
+      printf("%s instance", AS_INSTANCE(value)->class->name->chars);
       break;
 
     case OBJ_NATIVE:
