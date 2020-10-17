fae97a326ef22f8eac8aa5c47f45bbaf6c450dde
MichaelSDavid <349612929@gapps.yrdsb.ca>
2020-10-17 19:37:50 -0400

    Finalizing tests

--- src/scanner.c
+++ src/scanner.c
@@ -124,7 +124,7 @@ static TokenType identifierType() {
         switch (scanner.start[1]) {
           case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
           case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
-          case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+          case 'n': return checkKeyword(2, 1, "c", TOKEN_FUN);
         }
       }
       break;
